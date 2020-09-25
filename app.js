73e64d3f6b1ac3e1866480e936728cfd76434766
merima98 <merimaceranic98@gmail.com>
2020-09-25 15:35:40 +0200

    Static images & error handling

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const env = require('custom-env').env();
@@ -11,6 +12,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 const app = express();
 
 app.use(bodyParser.json()); //application/json
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -20,6 +22,14 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feetRoutes);
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({
+        message: message
+    });
+});
 
 mongoose.connect(
     MONGODB_URI
