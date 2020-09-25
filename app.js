11c13ba2e50f03f7d0a7f47c1e8c22cd66001bdf
merima98 <merimaceranic98@gmail.com>
2020-09-25 10:59:01 +0200

    REST APIs, Clients & CORS Errors | Sending POST request

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const app = express();
 
 app.use(bodyParser.json()); //application/json
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'Authorization');
+    next();
+});
+
 app.use('/feed', feetRoutes);
 
 app.listen(process.env.PORT);
