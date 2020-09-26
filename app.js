1ad672edc3022d8bfb0e8bfe5cf0f8e7554ae8ac
merima98 <merimaceranic98@gmail.com>
2020-09-26 20:25:22 +0200

    Uploading images into application

--- app.js
+++ app.js
@@ -3,15 +3,37 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const env = require('custom-env').env();
 const mongoose = require('mongoose');
-
+const multer = require('multer');
+// const { uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const feetRoutes = require('./routes/feed');
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const app = express();
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'images');
+    },
+    filename: function (req, file, cb) {
+        // cb(null, uuidv4())
+        cb(null, uuidv4() + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    }
+    else {
+        cb(null, false);
+    }
+};
+
 
 app.use(bodyParser.json()); //application/json
+app.use(multer({ storage: storage, fileFilter: fileFilter }).single('image'));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
--- controllers/feed.js
+++ controllers/feed.js
@@ -22,13 +22,19 @@ exports.createPost = (req, res, next) => {
         error.statusCode = 422;
         throw error;
     }
+    if (!req.file) {
+        const error = new Error('No image provided!');
+        error.statusCode = 422;
+        throw error;
+    }
 
+    const imageUrl = req.file.path.replace("\\", "/");
     const title = req.body.title;
     const content = req.body.content;
     const post = new Post({
         title: title,
         content: content,
-        imageUrl: 'images/cake.jpg',
+        imageUrl: imageUrl,
         creator: {
             name: 'Merima'
         }
