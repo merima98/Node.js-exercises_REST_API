e035782324a44d496451020cad17fdfa65b91147
merima98 <merimaceranic98@gmail.com>
2020-10-02 15:08:50 +0200

    Updating posts on all connected clients | sorting correctly

--- controllers/feed.js
+++ controllers/feed.js
@@ -15,6 +15,7 @@ exports.getPosts = async (req, res, next) => {
         const totalItems = await Post.find().countDocuments();
         const posts = await Post.find()
             .populate('creator')
+            .sort({ createdAt: -1 })
             .skip((currentPage - 1) * perPage)
             .limit(perPage);
 
@@ -126,13 +127,13 @@ exports.updatePost = async (req, res, next) => {
         throw error;
     }
     try {
-        const post = await Post.findById(postId);
+        const post = await (await Post.findById(postId)).populate('creator');
         if (!post) {
             const error = new Error('Could not find post.');
             error.statusCode = 404;
             throw error;
         }
-        if (post.creator.toString() !== req.userId) {
+        if (post.creator._id.toString() !== req.userId) {
             const error = new Error('Not authorized!');
             error.statusCode = 403;
             throw error;
@@ -144,7 +145,18 @@ exports.updatePost = async (req, res, next) => {
         post.imageUrl = imageUrl;
         post.content = content;
         const result = await post.save();
-        res.status(200).json({ message: 'Post updated!', post: result });
+        io.getIO().emit(
+            'posts',
+            {
+                action: 'update',
+                post: result
+            }
+        );
+        res.status(200).json(
+            {
+                message: 'Post updated!',
+                post: result
+            });
     }
     catch (err) {
         if (!err.statusCode) {
