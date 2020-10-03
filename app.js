d9931ebe89d48976bfede6b5ee1b2884ecf67efa
merima98 <merimaceranic98@gmail.com>
2020-10-03 17:45:11 +0200

    Adding a mutation resolver & GraphQL

--- app.js
+++ app.js
@@ -50,7 +50,8 @@ app.use((req, res, next) => {
 
 app.use('/graphql', graphqlHTTP({
     schema: graphqlSchema,
-    rootValue: graphqlResolver
+    rootValue: graphqlResolver,
+    graphiql: true
 }));
 
 
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,36 +1,41 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
-
     type Post {
-       _id: ID!
-       title: String! 
-       content: String! 
-       imageUrl: String! 
-       creator: User!
-       createdAt: String!
-       updatedAt: String!
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createdAt: String!
+        updatedAt: String!
     }
 
     type User {
         _id: ID!
-        email: String!
         name: String!
+        email: String!
         password: String
         status: String!
         posts: [Post!]!
     }
-     input userInputData {
+
+    input UserInputData {
         email: String!
         name: String!
         password: String!
-     }
-     type RootMutation {
-        createUser(userInput: userInputData): User!
-     }
-
-    schema {
-        mutation: RootMutation
     }
 
+    type RootQuery {
+        hello: String
+    }
+
+    type RootMutation {
+        createUser(userInput: UserInputData): User!
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
 `);
