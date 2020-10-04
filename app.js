1d94c72a93e4a0c9935f214e827ca7c30d0afeba
merima98 <merimaceranic98@gmail.com>
2020-10-04 22:53:27 +0200

    Handling errors

--- app.js
+++ app.js
@@ -51,7 +51,16 @@ app.use((req, res, next) => {
 app.use('/graphql', graphqlHTTP({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
-    graphiql: true
+    graphiql: true,
+    formatError(err) {
+        if (!err.originalError) {
+            return err;
+        }
+        const data = err.originalError.data;
+        const message = err.message || 'An error ocurred!';
+        const code = err.originalError.code || 500;
+        return { message: message, status: code, data: data };
+    }
 }));
 
 
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -17,6 +17,8 @@ module.exports = {
         }
         if (errors.length > 0) {
             const error = new Error('Invalid input!');
+            error.data = errors;
+            error.code = 422;
             throw error;
         }
 
