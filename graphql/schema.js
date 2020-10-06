cb844a13dbb926c5002c0145d96ed85cd015bab3
merima98 <merimaceranic98@gmail.com>
2020-10-06 22:25:34 +0200

    Sending Create post and Get post queries | Adding pagination

--- graphql/schema.js
+++ graphql/schema.js
@@ -25,6 +25,11 @@ module.exports = buildSchema(`
         userId: String!
     }
 
+    type PostData {
+        posts: [Post!]!
+        totalPosts: Int!
+    }
+
     input UserInputData {
         email: String!
         name: String!
@@ -39,12 +44,12 @@ module.exports = buildSchema(`
 
     type RootQuery {
         login(email: String!, password: String!): AuthData!
+        posts(page: Int): PostData!
     }
 
     type RootMutation {
         createUser(userInput: UserInputData): User!
         createPost(postInput: PostInputData): Post!
-
     }
 
     schema {
