f9e6bf7411ccdbd4c8a298ad3c5dde3976a4df3a
merima98 <merimaceranic98@gmail.com>
2020-10-07 10:51:49 +0200

    Uploading images | fetching the imageUrl | Viewing a single post | Updating posts | Deleting postd | Managing the user status | Using variables

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const path = require('path');
+const fs = require('fs');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const env = require('custom-env').env();
 const mongoose = require('mongoose');
 const multer = require('multer');
-// const { uuidv4 } = require('uuid');
 const { v4: uuidv4 } = require('uuid');
+
+const { clearImage } = require('./util/file');
 
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const graphqlSchema = require('./graphql/schema');
@@ -15,29 +18,34 @@ const auth = require('./middleware/auth');
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const app = express();
-const storage = multer.diskStorage({
+const fileStorage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'images');
     },
     filename: function (req, file, cb) {
-        // cb(null, uuidv4())
         cb(null, uuidv4() + '-' + file.originalname);
     }
 });
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
         cb(null, true);
-    }
-    else {
+    } else {
         cb(null, false);
     }
 };
 
-
-app.use(bodyParser.json()); //application/json
-app.use(multer({ storage: storage, fileFilter: fileFilter }).single('image'));
+app.use(bodyParser.json()); // application/json
+app.use(
+    multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
 app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -54,35 +62,51 @@ app.use((req, res, next) => {
 
 app.use(auth);
 
-app.use('/graphql', graphqlHTTP({
-    schema: graphqlSchema,
-    rootValue: graphqlResolver,
-    graphiql: true,
-    formatError(err) {
-        if (!err.originalError) {
-            return err;
-        }
-        const data = err.originalError.data;
-        const message = err.message || 'An error ocurred!';
-        const code = err.originalError.code || 500;
-        return { message: message, status: code, data: data };
+app.put('/post-image', (req, res, next) => {
+    if (!req.isAuth) {
+        throw new Error('Not authenticated!');
     }
-}));
+    if (!req.file) {
+        return res.status(200).json({ message: 'No file provided!' });
+    }
+    if (req.body.oldPath) {
+        clearImage(req.body.oldPath.replace("\\", "/"));
+    }
+    return res
+        .status(201)
+        .json({ message: 'File stored.', filePath: req.file.path.replace("\\", "/") });
 
+});
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: graphqlSchema,
+        rootValue: graphqlResolver,
+        graphiql: true,
+        formatError(err) {
+            if (!err.originalError) {
+                return err;
+            }
+            const data = err.originalError.data;
+            const message = err.message || 'An error occurred.';
+            const code = err.originalError.code || 500;
+            return { message: message, status: code, data: data };
+        }
+    })
+);
 
 app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
     const data = error.data;
-    res.status(status).json({
-        message: message,
-        data: data
-    });
+    res.status(status).json({ message: message, data: data });
 });
 
 mongoose.connect(
-    MONGODB_URI
+    MONGODB_URI,
+    { useUnifiedTopology: true }
 )
     .then(result => {
         app.listen(process.env.PORT);
@@ -90,3 +114,5 @@ mongoose.connect(
     .catch(err => {
         console.log(err)
     });
+
+
